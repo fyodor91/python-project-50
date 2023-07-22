c8e5cbbd61d1544a49124a008bc3b1e42c47d48e
fyodor91 <fyodorermakov91@gmail.com>
2023-07-22 14:42:05 +0700

    step 6.1

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,5 +1,6 @@
 from gendiff.generate import generate_diff
-from tests.fixtures.diff import true_result
+from tests.fixtures.diff import true_result as result_1
+from tests.fixtures.diff2 import true_result as result_2
 import pytest
 import json
 
@@ -7,10 +8,25 @@ import json
 def test_generate_json():
     file_1 = 'tests/fixtures/file1.json'
     file_2 = 'tests/fixtures/file2.json'
-    assert generate_diff(file_1, file_2) == true_result
+    assert generate_diff(file_1, file_2) == result_1
 
 
 def test_generate_yaml():
     file_1 = 'tests/fixtures/file1.yml'
     file_2 = 'tests/fixtures/file2.yml'
-    assert generate_diff(file_1, file_2) == true_result
+    assert generate_diff(file_1, file_2) == result_1
+
+
+def test_generate_json():
+    file_3 = 'tests/fixtures/file3.json'
+    file_4 = 'tests/fixtures/file4.json'
+    assert generate_diff(file_3, file_4) == result_2
+
+
+def test_generate_yaml():
+    file_3 = 'tests/fixtures/file3.yml'
+    file_4 = 'tests/fixtures/file4.yml'
+    assert generate_diff(file_3, file_4) == result_2
+
+
+
